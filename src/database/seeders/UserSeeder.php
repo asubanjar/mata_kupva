d910d49dd1f30c3beb3b1adb6a1051aa2122e129
Dimas Gilang Prayogo <dimas.prayogo@ppatk.go.id>
2023-08-28 04:25:08 +0000

    chore: fix setting

--- src/database/seeders/UserSeeder.php
+++ src/database/seeders/UserSeeder.php
@@ -9,25 +9,23 @@ class UserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
-     *
-     * @return void
      */
-    public function run()
+    public function run(): void
     {
-        $user = User::factory()->create([
-            'username'  => 'adminsikd',
-            'name'      => 'Administrator',
-            'email'     => 'admin@ppatk.go.id',
-            'password'  => bcrypt('secret'),
-            'phone'     => '6287812625858'
+        User::factory()->create([
+            'username' => 'adminsikd',
+            'name'     => 'Administrator',
+            'email'    => 'admin@ppatk.go.id',
+            'password' => bcrypt('secret'),
+            'phone'    => '6287812625858',
         ]);
 
-        $user = User::factory()->create([
-            'username'  => 'nxs005',
-            'name'      => 'Nurman Setyawan',
-            'email'     => 'nurman.setyawan@ppatk.go.id',
-            'password'  => bcrypt('secret'),
-            'phone'     => '6287812625858'
+        User::factory()->create([
+            'username' => 'nxs005',
+            'name'     => 'Nurman Setyawan',
+            'email'    => 'nurman.setyawan@ppatk.go.id',
+            'password' => bcrypt('secret'),
+            'phone'    => '6287812625858',
         ]);
     }
 }
